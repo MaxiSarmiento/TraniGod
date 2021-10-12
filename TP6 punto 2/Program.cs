24f925c4de7e034b922e4decdb8649686b23076c
MaxiSarmiento <92354282+MaxiSarmiento@users.noreply.github.com>
2021-10-12 02:23:47 -0300

    Add files via upload

---
+++ TP6 punto 2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System;
+// Desarrollar un programa que
+// permita registrar información de película
+// s: nombre, director, año estreno y genero.
+// La información se debe almacenar en un archivo de texto.
+// Mediante un menú de usuario se debe permitir grabar nuevas películas y
+// cceder al listado completo de películas grabadas.
+
+
+namespace TP6_punto_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al sistema de peliculITES , señale la accion a realizar :\n" +
+          "\n1.- Guardar Película" +
+              "\n2.- Ver peliculas almacenadas"
+              );
+
+            string s1 = null;
+
+            s1 = Console.ReadLine();
+
+            switch (s1)
+            {
+                case "1":
+                    Console.WriteLine("Indique los datos a almacenar\n");
+                    int numero = 2;
+                    numero = numero + 1;
+                    string año = "";
+                    string nombre = "";
+                    string director = "";
+                    string genero = "";
+                    string ruta = @"D:\peliculasTP.txt";
+
+                    System.Console.WriteLine("Ingrese el  nombre de la pelicula");
+                   nombre = Console.ReadLine();
+                    System.Console.WriteLine("Ingrese el director de la pelicula");
+                    director = Console.ReadLine();
+                    System.Console.WriteLine("Ingrese el año de la pelicula");
+                    año = Console.ReadLine();
+                    System.Console.WriteLine("Ingrese el/los generos (seguidos por coma) de la pelicula");
+                    genero = Console.ReadLine();
+                    StreamWriter writer = File.AppendText(ruta);
+
+                    // agragamos contenido
+                    writer.WriteLine("\n#{0}", numero);
+                    writer.WriteLine("Nombre:"); writer.WriteLine(nombre);
+                    writer.WriteLine("Director:"); writer.WriteLine(director);
+                    writer.WriteLine("Año:"); writer.WriteLine(año);
+                    writer.WriteLine("Género"); writer.WriteLine(genero);
+                    writer.Close();
+
+                    
+
+
+                    break;
+                case "2":
+                    Console.WriteLine("Las peliculas listadas son:\n"); {
+
+
+                        string rutaCompleta = @"D:\peliculasTP.txt";
+
+                        string line = "";
+                        using (StreamReader file = new StreamReader(rutaCompleta))
+                        {
+                            while ((line = file.ReadLine()) != null)                //Leer linea por linea
+                            {
+                                Console.WriteLine(line);
+                            }
+
+                        }
+                        break;
+                                           
+
+
+                    }
+            }
+        }
+    }
+}
---
+++ Tp6 punto 1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+//Elaborar un programa que permita registrar las temperaturas para un periodo dado (quincenal o mensual) y al finalizar informe la temperatura promedio, máxima y mínima. Se requiere utilizar un arreglo (vector) para almacenar las temperaturas.
+
+
+namespace Tp6_punto_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int n = 0;
+            int dia = 1;
+        int maxima;
+        int minima;
+        string valor = "";
+        float minimo = 15.0f;
+        float maximo = 30.0f;
+        float suma = 0.0f;
+        float promedio = 0.0f;
+           
+        
+            Console.WriteLine("Bienvenido al sistema, elija que tipo de registro quiere realizar:\n" +
+            "\n1.- Quincenal" +
+                "\n2.- Mensual"  
+                );
+
+           string s1 = null;
+
+            s1 = Console.ReadLine();
+
+            switch (s1)
+            {
+                case "1":
+                    Console.WriteLine("usted eligió quincenal");
+                    float[] quincenal = new float[15];
+                    for (n = 0; n < 15; n++)
+                    {
+                        Console.WriteLine("ingrese la temperatura del dia: {0}\n",dia);
+                        dia = dia + 1;
+                        valor = Console.ReadLine();
+                        quincenal[n] = Convert.ToSingle(valor);
+
+                    }
+                    for (n = 0; n < 15; n++)
+                    {
+                       suma = suma + quincenal[n];
+                    }
+                     promedio = suma / 15;
+                    for (n = 0; n < 15; n++)
+                    {
+                        if (quincenal[n] > maximo)
+                        {
+                            maximo = quincenal[n];
+                        }
+                    }
+                    for (n = 0; n < 15; n++)
+                    {
+                        if (quincenal[n] < minimo)
+                        {
+                            minimo = quincenal[n];
+                        }
+                    }
+                    Console.Clear();
+                    Console.WriteLine("El promedio de temperatura es {0}", Math.Round(promedio, 2));
+                    Console.WriteLine("La temperatura máxima es {0}", maximo);
+                    Console.WriteLine("La temperatura mínima es {0}", minimo);
+
+                    Console.WriteLine("Presione una tecla para finalizar...");
+                    Console.ReadKey();
+            
+            break;
+                case "2":
+                    Console.WriteLine("Usted elgió Mensual");
+                    float[] mensual = new float[31];
+                    for (n = 0; n < 31; n++)
+                    {
+                        Console.WriteLine("ingrese la temperatura del dia: {0}\n",dia);
+                        dia = dia + 1;
+                        valor = Console.ReadLine();
+                        mensual[n] = Convert.ToSingle(valor);
+                    }
+                    for (n = 0; n < 31; n++)
+                    {
+                        suma = suma + mensual[n];
+                    }
+                    promedio = suma / 31;
+                    for (n = 0; n < 31; n++)
+                    {
+                        if (mensual[n] > maximo)
+                        {
+                            maximo = mensual[n];
+                        }
+                    }
+                    for (n = 0; n < 31; n++)
+                    {
+                        if (mensual[n] < minimo)
+                        {
+                            minimo = mensual[n];
+                        }
+                    }
+                    Console.Clear();
+                    Console.WriteLine("El promedio de temperatura es: {0}", Math.Round(promedio, 2));
+                    Console.WriteLine("La temperatura máxima es {0}", maximo);
+                    Console.WriteLine("La temperatura mínima es {0}", minimo);
+
+                    Console.WriteLine("Presione una tecla para finalizar...");
+                    Console.ReadKey();
+
+                    break;
+                default:
+                    Console.WriteLine("opcion no válida, seleccione una opcion");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
